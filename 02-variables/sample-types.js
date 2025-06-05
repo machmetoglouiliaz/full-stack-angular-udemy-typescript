749d86da5a81564e52f387ab5cbb59be7283608b
Mourat Achmet oglou Iliaz <mayrit91@gmail.com>
2025-06-05 16:28:07 +0300

    Section 4: Hello world + variables + git ignore for VS files

---
+++ 02-variables/sample-types.js
@@ -0,0 +1,9 @@
+var found = true;
+var grade = 88.6;
+var firstName = "Mourat";
+var lastName = 'Achoi';
+console.log(found);
+console.log("The grade is " + grade);
+console.log("Hi " + firstName + " " + lastName);
+// Use template string
+console.log("Hi ".concat(firstName, " ").concat(lastName));
---
+++ 02-variables/sample-types.ts
@@ -0,0 +1,11 @@
+let found: boolean = true;
+let grade: number = 88.6;
+let firstName: String = "Mourat";
+let lastName: String = 'Achoi';
+
+console.log(found);
+console.log("The grade is " + grade);
+console.log("Hi " + firstName + " " + lastName);
+
+// Use template string
+console.log(`Hi ${firstName} ${lastName}`);
